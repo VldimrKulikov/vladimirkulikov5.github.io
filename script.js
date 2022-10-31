8057bf95a2826e89daf48ae8ba1ca3b065aedf9a
VldimrKulikov <112845661+VldimrKulikov@users.noreply.github.com>
2022-10-31 08:46:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
-$(function(){
-
-	$('.slid').slick({
-			dots: true,
-			infinite: false,
-			slidesToShow: 4,
-			slidesToScroll: 1,
-			speed: 100,
-			responsive:[
-
-					{
-					breakpoint: 480,
-					settings:{
-							slidesToShow: 2,
-							slidesToScroll: 1,
-							arrows:false,
-					}
-					}
-			]
-	});
-})
+function multiply(){
+	var num1,num2,result;
+	num1=document.getElementById('n1').value;
+	num1=parseInt(num1);
+	num2=document.getElementById('n2').value;
+	num2=parseInt(num2);
+	result=num1*num2;
+	if (isNaN(result)){
+		result="Ошибка";
+	document.getElementById('out').innerHTML=result;}
+	document.getElementById('out').innerHTML=result;
+}
